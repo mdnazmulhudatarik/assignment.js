222bc9b8cb72bc0469fc468c3912c103ebc06b91
Md Nazmul Huda Tarik <mdnazmulhudatarik@gmail.com>
2021-01-22 23:11:36 +0600

    kilometerToMeter Project

--- assignment.js
+++ assignment.js
@@ -1,1 +1,13 @@
 // https://github.com/mdnazmulhudatarik/assignment.js
+
+
+
+// kilometerToMeter Convert
+
+function kilometerToMeter(n) {
+    var meterCalculate = n * 1000;
+    return meterCalculate;
+}
+
+var result = (kilometerToMeter());
+console.log(result);
